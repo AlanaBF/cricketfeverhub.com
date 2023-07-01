dbad7ec22901784c44d626dc5dd54bb82a9c8b4a
Alana Barrett-Frew <alanabarrett-frew@hotmail.com>
2023-07-01 13:36:14 +0100

    image loading added to news article to remove bug

--- src/components/News/CricketCardLayout.jsx
+++ src/components/News/CricketCardLayout.jsx
@@ -12,6 +12,7 @@ const CricketCardLayout = () => {
   const [selectedArticle, setSelectedArticle] = useState(null);
   const [showModal, setShowModal] = useState(false);
   const [articleContent, setArticleContent] = useState("");
+  const [isLoadingImage, setIsLoadingImage] = useState(false); // Added loading state
 
   useEffect(() => {
     const fetchData = async () => {
@@ -35,9 +36,12 @@ const CricketCardLayout = () => {
   useEffect(() => {
     const fetchCoverImage = async () => {
       try {
+        setIsLoadingImage(true); // Set loading state
         const data = await getImages(selectedArticle.coverImage.id);
+        setIsLoadingImage(false); // Clear loading state
       } catch (error) {
         console.error("Error:", error);
+        setIsLoadingImage(false); // Clear loading state in case of error
       }
     };
 
@@ -108,11 +112,17 @@ const CricketCardLayout = () => {
           </Modal.Header>
           <Modal.Body>
             {selectedArticle.coverImage && (
-              <Card.Img
-                variant="top"
-                src={`https://cricbuzz-cricket.p.rapidapi.com/img/v1/i1/c${selectedArticle.coverImage.id}/i.jpg?p=de`}
-                alt="Cover Image"
-              />
+              <>
+                {isLoadingImage ? (
+                  <p>Loading image...</p> // Show loading state
+                ) : (
+                  <Card.Img
+                    variant="top"
+                    src={`https://cricbuzz-cricket.p.rapidapi.com/img/v1/i1/c${selectedArticle.coverImage.id}/i.jpg?p=de`}
+                    alt="Cover Image"
+                  />
+                )}
+              </>
             )}
             {articleContent && articleContent.length > 0 ? (
               articleContent.map((item, index) => (
@@ -138,130 +148,3 @@ const CricketCardLayout = () => {
 };
 
 export default CricketCardLayout;
-
-
-// import React, { useEffect, useState } from "react";
-// import getNewsData from "../../utils/getNews_API";
-// import Card from "react-bootstrap/Card";
-// import Modal from "react-bootstrap/Modal";
-// import Button from "react-bootstrap/Button";
-// import "../../assets/styles/components.css";
-// import axios from "axios";
-// import getImages from "../../utils/getImage_API";
-// import NewsArticle from "./NewsArticle"; // Import the NewsArticle component
-
-// const CricketCardLayout = () => {
-//   const [news, setNews] = useState([]);
-//   const [selectedArticle, setSelectedArticle] = useState(null);
-//   const [showModal, setShowModal] = useState(false);
-//   const [articleContent, setArticleContent] = useState("");
-//   const [imageURL, setImageURL] = useState("");
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         const newsData = await getNewsData();
-//         const topStories = newsData.storyList.slice(0, 11);
-//         setNews(topStories);
-//       } catch (error) {
-//         console.error("Error fetching cricket news data:", error);
-//       }
-//     };
-
-//     fetchData();
-//   }, []);
-
-//   const formatTimestamp = (timestamp) => {
-//     const date = new Date(parseInt(timestamp));
-//     return date.toLocaleString();
-//   };
-
-//   const handleOpenModal = async (item) => {
-//     setSelectedArticle(item);
-//     setShowModal(true);
-
-//     const VITE_RapidAPI_Key = import.meta.env.VITE_RapidAPI_Key;
-//     try {
-//       const response = await axios.get(
-//         `https://cricbuzz-cricket.p.rapidapi.com/news/v1/detail/${item.id}`,
-//         {
-//           headers: {
-//             "x-rapidapi-host": "cricbuzz-cricket.p.rapidapi.com",
-//             "x-rapidapi-key": VITE_RapidAPI_Key,
-//           },
-//         }
-//       );
-//       const data = response.data;
-//       console.log("data.content:", data.content); // Add this line
-
-//       setArticleContent(data.content);
-
-//       if (item.coverImage && item.coverImage.id) {
-//         try {
-//           const imageData = await getImages(item.coverImage.id);
-//           setImageURL(imageData.url);
-//         } catch (error) {
-//           console.error("Error fetching image:", error);
-//           setImageURL("");
-//         }
-//       } else {
-//         setImageURL("");
-//       }
-//     } catch (error) {
-//       console.error("Error fetching article content:", error);
-//     }
-//   };
-
-//   const handleCloseModal = () => {
-//     setShowModal(false);
-//     setArticleContent("");
-//     setImageURL("");
-//     setSelectedArticle(null);
-//   };
-
-//   return (
-//     <div className="news-card-layout">
-//       {news.map((item) => {
-//         if (item && item.story) {
-//           const { id, hline, intro, source, pubTime } = item.story;
-
-//           return (
-//             <Card key={id} className="card">
-//               <Card.Body>
-//                 <Card.Title>{hline}</Card.Title>
-//                 <Card.Text>{intro}</Card.Text>
-//                 <Card.Text>Source: {source}</Card.Text>
-//                 <Card.Text>
-//                   Publication Time: {formatTimestamp(pubTime)}
-//                 </Card.Text>
-//                 <Button onClick={() => handleOpenModal(item.story)}>
-//                   Read More
-//                 </Button>
-//               </Card.Body>
-//             </Card>
-//           );
-//         } else {
-//           return null;
-//         }
-//       })}
-
-//       {selectedArticle && (
-//         <Modal show={showModal} onHide={handleCloseModal}>
-//           <Modal.Header closeButton>
-//             <Modal.Title>{selectedArticle.headline}</Modal.Title>
-//           </Modal.Header>
-//           <Modal.Body>
-//             <NewsArticle articleContent={articleContent} />
-//           </Modal.Body>
-//           <Modal.Footer>
-//             <Button variant="secondary" onClick={handleCloseModal}>
-//               Close
-//             </Button>
-//           </Modal.Footer>
-//         </Modal>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default CricketCardLayout;
