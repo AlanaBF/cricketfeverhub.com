e5ab4e946fd4932381c77476edc477ea836bdf79
Alana Barrett-Frew <alanabarrett-frew@hotmail.com>
2023-07-06 13:00:00 +0100

    timestamp removed

--- src/components/Comms/MatchComms.jsx
+++ src/components/Comms/MatchComms.jsx
@@ -17,16 +17,6 @@ const MatchCommentary = ({ matchId }) => {
     fetchCommentaryData();
   }, [matchId]);
 
-  const timestampToTimeString = (timestamp) => {
-    const date = new Date(timestamp * 1000);
-    const hours = date.getHours();
-    const minutes = date.getMinutes();
-    const ampm = hours >= 12 ? 'PM' : 'AM';
-    const formattedHours = hours % 12 === 0 ? 12 : hours % 12;
-    const formattedMinutes = minutes < 10 ? '0' + minutes : minutes;
-    return `${formattedHours}:${formattedMinutes} ${ampm}`;
-  };
-
   if (error) {
     return <div>Error: {error}</div>;
   }
@@ -39,7 +29,6 @@ const MatchCommentary = ({ matchId }) => {
         commentaryData.commentaryList.map((commentary, index) => (
           <div key={index}>
             <p>{commentary.commText}</p>
-            <p>Timestamp: {timestampToTimeString(commentary.timestamp)}</p>
           </div>
         ))}
         
