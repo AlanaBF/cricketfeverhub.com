b76bf8767d934f314c2b10eeb847393f44e5ecdd
Alana Barrett-Frew <alanabarrett-frew@hotmail.com>
2023-07-26 20:56:04 +0100

    weather api and leaflet api linked for the maps

--- src/utils/Leaflet/Leaflet_API.jsx
+++ src/utils/Leaflet/Leaflet_API.jsx
@@ -1,37 +1,76 @@
+import React, { useState, useEffect } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import axios from "axios";
+import L from "leaflet";
 
 import "../../assets/styles/components.css";
 
-// Create a custom marker icon with a Font Awesome icon
-const customIcon = L.divIcon({
-  className: "custom-icon",
-  html: '<i class="fas fa-location-dot"></i>',
-});
+const Map = ({ venue }) => {
+  const [weatherOverlay, setWeatherOverlay] = useState(null);
+  const [selectedLayer, setSelectedLayer] = useState("precipitation_new"); // Default selected layer
 
-function Map({ venue }) {
+  const layers = [
+    { value: "precipitation_new", label: "Precipitation" },
+    // { value: "temp_new", label: "Temperature" },
+    // Add more layers as needed
+  ];
+
+  const fetchWeatherOverlay = () => {
+    const apiKey = "399a31ebf2dcefc84c5cf743172caaf6";
+    const url = `https://tile.openweathermap.org/map/${selectedLayer}/{z}/{x}/{y}.png?appid=${apiKey}`;
+
+      setWeatherOverlay(url);
+  };
+
+  useEffect(() => {
+    fetchWeatherOverlay();
+  }, [selectedLayer]);
+
+  const customIcon = L.divIcon({
+    className: "custom-icon",
+    html: '<i class="fas fa-location-dot"></i>',
+  });
+
   return (
-    <MapContainer
-      center={[venue.latitude, venue.longitude]}
-      zoom={15}
-      scrollWheelZoom={true}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
+    <div>
+      <div className="layer-selector">
+        {layers.map((layer) => (
+          <label key={layer.value}>
+            <input
+              type="radio"
+              value={layer.value}
+              checked={selectedLayer === layer.value}
+              onChange={() => setSelectedLayer(layer.value)}
+            />
+            {layer.label}
+          </label>
+        ))}
+      </div>
 
-      <Marker position={[venue.latitude, venue.longitude]}>
-        <Popup>
-          <div className="card">
-            <h3>{venue.ground}</h3>
-            <h4>{venue.city}</h4>
-          </div>
-        </Popup>
-      </Marker>
-    </MapContainer>
+      <MapContainer
+        center={[venue.latitude, venue.longitude]}
+        zoom={15}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
+        {weatherOverlay && <TileLayer url={weatherOverlay} />}
+
+        <Marker position={[venue.latitude, venue.longitude]} icon={customIcon}>
+          <Popup>
+            <div className="card">
+              <h3>{venue.ground}</h3>
+              <h4>{venue.city}</h4>
+            </div>
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
   );
-}
+};
 
-
-export default Map
+export default Map;
--- src/utils/Weather/index.jsx
+++ src/utils/Weather/index.jsx
@@ -1,4 +1,3 @@
-// https://tile.openweathermap.org/map/{layer}/{z}/{x}/{y}.png?appid={API key}
 
 function displayWeatherInfo(city) {
     var APIKey = "06049584939bfdb947b35800e5407bab";
@@ -22,96 +21,7 @@ function displayWeatherInfo(city) {
     $("#humidity").text("Humidity: " + humidity + "%")
 
 
-
-
-
     }
     )
 }
 
-
-// import React, { useRef, useEffect, useState } from 'react';
-// import 'leaflet/dist/leaflet.css';
-// import L from 'leaflet';
-// import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-
-// import "../../assets/styles/components.css";
-
-// // Create a custom marker icon with a Font Awesome icon
-// const customIcon = L.divIcon({
-//   className: "custom-icon",
-//   html: '<i class="fas fa-location-dot"></i>',
-// });
-
-// function CricketMap({ venue }) {
-//   const mapRef = useRef(null);
-//   const [selectedLayer, setSelectedLayer] = useState('precipitation_new'); // Default layer
-
-//   useEffect(() => {
-//     // Initialize the map
-//     const map = mapRef.current;
-
-//     // Add the initial layer to the map
-//     const VITE_RapidAPI_KeyWeather = import.meta.env.VITE_RapidAPI_KeyWeather;
-
-//     L.tileLayer(`https://tile.openweathermap.org/map/${precipitation_new}/{z}/{x}/{y}.png?appid=${VITE_RapidAPI_KeyWeather}`, {
-//       maxZoom: 19,
-//       attribution: '© OpenWeatherMap contributors',
-//     }).addTo(map);
-
-//     // Add markers or other map features as needed
-
-//     // Clean up the map when the component is unmounted
-//     return () => {
-//       map.remove();
-//     };
-//   }, [venue.latitude, venue.longitude, selectedLayer]);
-
-//   const handleLayerChange = (layer) => {
-//     setSelectedLayer(layer);
-//   };
-
-//   return (
-//     <div>
-//       {/* Map */}
-//       <MapContainer
-//         center={[venue.latitude, venue.longitude]}
-//         zoom={15}
-//         scrollWheelZoom={true}
-//         ref={mapRef}
-//       >
-//         <TileLayer
-//           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-//           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-//         />
-
-//         <Marker position={[venue.latitude, venue.longitude]}>
-//           <Popup>
-//             <div className="card">
-//               <h3>{venue.ground}</h3>
-//               <h4>{venue.city}</h4>
-//               {/* Additional venue information */}
-//             </div>
-//           </Popup>
-//         </Marker>
-//       </MapContainer>
-
-//       {/* Section box with layer selection */}
-//       <div>
-//         <h3>Select Layer:</h3>
-//         <label>
-//           <input
-//             type="radio"
-//             name="layer"
-//             value="precipitation_new"
-//             checked={selectedLayer === 'precipitation_new'}
-//             onChange={() => handleLayerChange('precipitation_new')}
-//           />
-//           Precipitation
-//         </label>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default CricketMap;
