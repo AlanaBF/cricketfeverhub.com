6a1690b9555cf6df048de091c9c7e3ed17e9ab90
Aleksandr Riabov <sania.riabov@gmail.com>
2023-08-02 23:17:39 +0100

    Changed API url to version-2

--- src/utils/getScorecard_API.jsx
+++ src/utils/getScorecard_API.jsx
@@ -4,7 +4,7 @@ const getScorecard = async (matchId) => {
   const VITE_RapidAPI_Key = import.meta.env.VITE_RapidAPI_Key;
   const options = {
     method: 'GET',
-    url: `https://cricbuzz-cricket.p.rapidapi.com/mcenter/v1/${matchId}/scard`,
+    url: `https://cricbuzz-cricket.p.rapidapi.com/mcenter/v1/${matchId}/hscard`,
     headers: {
       'X-RapidAPI-Key': VITE_RapidAPI_Key,
       'X-RapidAPI-Host': 'cricbuzz-cricket.p.rapidapi.com'
