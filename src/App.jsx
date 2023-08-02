b3c2408de1a7266077bc39945e7ce5b9a441ec3a
Alana Barrett-Frew <alanabarrett-frew@hotmail.com>
2023-08-02 21:01:06 +0100

    HashRouter added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { createHashRouter, createRoutesFromElements, RouterProvider, Outlet, Route } from "react-router-dom";
+
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Home from "./pages/Home/Home";
@@ -12,25 +14,36 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 
 const App = () => {
-  return (
-    <Router>
-      {/* Header component */}
-      <Header />
-
-      {/* Routes */}
-      <Routes>
+    const router = createHashRouter(
+      createRoutesFromElements(
+        <Route path="/" element={<Root />}>
         <Route path="/" element={<Home />} />
         <Route path="/TeamSelectionPage" element={<TeamSelectionPage />} />
         <Route path="/LiveMatchesPage" element={<LiveMatchesPage />} />
         <Route path="/scorecard/:matchId" element={<ScoreCardPage />} />
         <Route path="/UpcomingMatchesPage" element={<UpcomingMatchesPage />} />
         <Route path="*" element={<NotFound />} />
-      </Routes>
+        </Route>
+  )
+  )
+  return <RouterProvider router={router} />
+}
 
-      {/* Footer component */}
-      <Footer />
-    </Router>
-  );
-};
+const Root = () => {
+  return (
+    <>
+      <div>
+        <Header />
+      </div>
+      <div>
+        <Outlet />
+      </div>
+      <div>
+        <Footer />
+      </div>
+    </>
+  )
+}
 
-export default App;
+
+export default App
