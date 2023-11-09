9ef962d2a2fbd661b03ae9999bb0bca6e4cf8bdb
Alana Barrett-Frew <alanabarrett-frew@hotmail.com>
2023-11-09 11:40:04 +0000

    vercel analytics added

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
-
+import { inject } from '@vercel/analytics';
+ 
+inject();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App />
